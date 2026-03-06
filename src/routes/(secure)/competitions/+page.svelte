12c91fbe4dbb0472a1db46252b72fb86ebe06626
Richard Herbert <richard@infoweb.co.uk>
2026-03-06 12:01:44 +0000

    Updated display tables

--- src/routes/(secure)/competitions/+page.svelte
+++ src/routes/(secure)/competitions/+page.svelte
@@ -1,11 +1,50 @@
 <script>
 	let { data } = $props();
+
+	async function deleteRecord( record ) {
+		const isConfirmed = window.confirm( `Are you sure you want to delete "${record.identifier}?"` );
+
+		if ( isConfirmed ) {
+			const formData = new FormData();
+
+			formData.append( 'id', record.id );
+
+			const response = await fetch( '?/delete', {
+				method: 'POST',
+				body: formData,
+			} );
+
+			location.href = '/competitions';
+		}
+	}
 </script>
 
 <h1>Competitions</h1>
 
-<ul>
-	{#each data.competitions as competition}
-		<li>{ competition.id } { competition.identifier } { competition.gender } { competition.ageOver }</li>
-	{/each}
-</ul>
+<figure>
+	<table>
+		<thead>
+			<tr>
+				<th>Identifier</th>
+				<th>Gender</th>
+				<th>Age Over</th>
+				<th></th>
+			</tr>
+		</thead>
+
+		<tbody>
+			{#each data.competitions as competition}
+				<tr>
+					<td>
+						<a href="/competitions/{ competition.id }">{ competition.identifier }</a>
+					</td>
+					<td>{ competition.gender }</td>
+					<td>{ competition.ageOver }</td>
+					<td class="align-right">
+						<button onclick={ () => deleteRecord( competition ) }>Delete</button>
+					</td>
+				</tr>
+			{/each}
+		</tbody>
+	</table>
+</figure>
--- src/routes/(secure)/tournaments/+page.svelte
+++ src/routes/(secure)/tournaments/+page.svelte
@@ -1,11 +1,57 @@
 <script>
 	let { data } = $props();
+
+	const formatDate = new Intl.DateTimeFormat( 'en-GB', {
+		day: 'numeric',
+		month: 'short',
+		year: 'numeric'
+	} );
+
+	async function deleteRecord( record ) {
+		const isConfirmed = window.confirm( `Are you sure you want to delete "${record.name}?"` );
+
+		if ( isConfirmed ) {
+			const formData = new FormData();
+
+			formData.append( 'id', record.id );
+
+			const response = await fetch( '?/delete', {
+				method: 'POST',
+				body: formData,
+			} );
+
+			location.href = '/tournaments';
+		}
+	}
 </script>
 
 <h1>Tournaments</h1>
 
-<ul>
-	{#each data.tournaments as tournament}
-		<li>{ tournament.id } { tournament.name }</li>
-	{/each}
-</ul>
+<figure>
+	<table>
+		<thead>
+			<tr>
+				<th>Tournament</th>
+				<th>Date</th>
+				<th>Country</th>
+				<th></th>
+			</tr>
+		</thead>
+
+		<tbody>
+			{#each data.tournaments as tournament}
+				<tr>
+					<td>
+						<a href="/tournaments/{ tournament.id }">{ tournament.name }</a>
+						<a href="{ tournament.website}" target="_blank" rel="noopener noreferrer">link</a>
+					</td>
+					<td>{ formatDate.format( new Date( tournament.dateStart ) ) } - { formatDate.format( new Date( tournament.dateEnd ) ) }</td>
+					<td>{ tournament.country }, { tournament.location }</td>
+					<td class="align-right">
+						<button onclick={ () => deleteRecord( tournament ) }>Delete</button>
+					</td>
+				</tr>
+			{/each}
+		</tbody>
+	</table>
+</figure>
