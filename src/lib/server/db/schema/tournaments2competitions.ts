64f93abce8d5e995ea8ec1e6db558d027d566f1f
Richard Herbert <richard@infoweb.co.uk>
2026-03-08 19:47:45 +0000

    Added cascade deletes on tournaments

--- src/lib/server/db/schema/tournaments2competitions.ts
+++ src/lib/server/db/schema/tournaments2competitions.ts
@@ -4,27 +4,13 @@ import { tournaments } from './tournaments';
 import { competitions } from './competitions';
 
 export const tournaments2competitions = mysqlTable( 'tournaments2competitions', {
-	tournamentID: varchar( 'tournamentID', { length: 255 } ).notNull().references( () => tournaments.id),
+	tournamentID: varchar( 'tournamentID', { length: 255 } ).notNull().references(() => tournaments.id, { onDelete: 'cascade' } ),
 	competitionID: varchar( 'competitionID', { length: 255 } ).notNull().references( () => competitions.id),
 	old_tournamentID: char( 'old_tournamentID', { length: 36 } ),
 	old_competitionID: char( 'old_competitionID', { length: 36 } ),
 }, ( table ) => [
 	primaryKey( { columns: [ table.tournamentID, table.competitionID ] } ),
 ] );
-
-/* export const relations = defineRelations({ tournaments, competitions, tournaments2competitions },
-	(r) => ({
-		tournaments: {
-		competitions: r.many.competitions({
-			from: r.tournaments.id.through(r.tournaments2competitions.tournamentID),
-			to: r.competitions.id.through(r.tournaments2competitions.competitionID),
-		}),
-		},
-		competitions: {
-		participants: r.many.tournaments(),
-		},
-	})
-); */
 
 export const tournamentsRelations = relations( tournaments, ( { many } ) => ( {
 	tournaments2competitions: many( tournaments2competitions ),
