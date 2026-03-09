8763146939479e1ee739520cc195e04117bad365
Richard Herbert <richard@infoweb.co.uk>
2026-03-09 12:36:47 +0000

    Refactored schema locations

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from 'drizzle-kit';
 
 if ( !process.env.DATABASE_URL ) throw new Error( 'DATABASE_URL is not set' );
 
-export default defineConfig({
-	schema: './src/lib/server/db/schema.ts',
+export default defineConfig( {
+	schema: './src/lib/server/db/schema/*',
 	dialect: 'mysql',
 	dbCredentials: { url: process.env.DATABASE_URL },
 	verbose: true,
---
+++ src/lib/server/db/schema/index.ts
@@ -0,0 +1,5 @@
+export * from './auth.schema';
+
+export * from './tournaments';
+export * from './competitions';
+export * from './tournaments2competitions';
