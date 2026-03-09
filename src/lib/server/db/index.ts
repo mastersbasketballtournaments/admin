8234a0b4d467be30dedcd99353c02f7165fbd61a
Richard Herbert <richard@infoweb.co.uk>
2026-03-09 15:13:56 +0000

    Added support for Vercel deployment

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -3,8 +3,12 @@ import mysql from 'mysql2/promise';
 import * as schema from './schema';
 import { env } from '$env/dynamic/private';
 
-if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
+if ( !env.DATABASE_URL ) throw new Error( 'DATABASE_URL is not set' );
 
-const client = mysql.createPool(env.DATABASE_URL);
+const client = mysql.createPool( {
+		uri: env.DATABASE_URL,
+		connectionLimit: 1,  // keep it low for serverless
+		waitForConnections: true,
+} );
 
-export const db = drizzle(client, { schema, mode: 'default' });
+export const db = drizzle( client, { schema, mode: 'default' } );
