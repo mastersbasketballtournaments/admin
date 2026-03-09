f3059201bbc3f71dd7b406d8a6b78b7d13442e3b
Richard Herbert <richard@infoweb.co.uk>
2026-03-09 11:50:50 +0000

    Refactored nav to be dynamic

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,40 @@
 <script lang="ts">
 	import favicon from '$lib/assets/favicon.svg';
+	import { enhance } from '$app/forms';
 
-	let { children } = $props();
+	let { data, children } = $props();
 </script>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
-{@render children()}
+<header>
+	<h1>Masters Basketball Tournaments</h1>
+
+	<nav>
+		<ul>
+			{#if data.user}
+				<!-- <li><a class="current" href="/">Home</a></li> -->
+
+				<li><a href="/dashboard">Dashboard</a></li>
+				<li><a href="/tournaments">Tournaments</a></li>
+				<li><a href="/competitions">Competitions</a></li>
+				<li><a href="/register">Register</a></li>
+				<li>
+					<form method="post" action="/?/signOut" use:enhance>
+						<button>Sign out</button>
+					</form>
+				</li>
+			{:else}
+				<li><a href="/signin">Sign In</a></li>
+			{/if}
+		</ul>
+	</nav>
+</header>
+
+<main>
+	{@render children()}
+</main>
+
+<footer></footer>
